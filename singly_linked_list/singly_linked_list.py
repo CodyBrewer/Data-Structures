09a87197ab2c7fe3e8da0cda6e8aa8fef667d502
Cody Brewer <codybrewerdev@gmail.com>
2020-08-19 19:06:02 -0600

    finish singly_linked_list

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -7,15 +7,72 @@ class LinkedList:
     def __init__(self):
         self.head:Node = None # Stores a node, that corresponds to our first node in the lsit
         self.tail:Node = None# Stores a node, that corresponds to our last node in the lsit
-    
+
     def add_to_head(self, value):
-        pass
+        # Create a node to add
+        new_node = Node(value)
+        # check if list is empty
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # new__node should point to current head
+            new_node.next_node = self.head
+            self.head = new_node
 
     def add_to_tail(self, value):
-        pass
+        # Create a node to add
+        new_node = Node(value)
+        # check if list is empty
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # point the node at the current tail, to the new node
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def remove_head(self):
-        pass
-
+        # if list is empty do nothing
+        if not self.head:
+            return None
+        # if lsit only has one element, set head and tail to None
+        if self.head.next_node is None:
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
+        # otherwise we have more elementts in the lsit
+        head_value = self.head.value
+        self.head = self.head.next_node
+        return head_value
+    
+    def remove_tail(self):
+        # if list is empty do nothing
+        if not self.head:
+            return None
+        if self.head.next_node is None:
+            tail_value = self.tail.value
+            self.tail = None
+            self.head = None
+            return tail_value
+        current = self.head
+        tail_value = self.tail.value
+        while current.next_node != self.tail:
+            current = current.next_node
+        current.next_node = None
+        self.tail = current
+        return tail_value
     def contains(self, value):
-        pass
+        if self.head is None:
+            return False
+
+        # Loop through each node, until we see the value, or we cannot go further
+        current_node = self.head
+
+        while current_node is not None:
+            # check if this is the ndoe we are look for 
+            if current_node.value == value:
+                return True
+            current_node = current_node.next_node
+        return False    
